d1f63b24589f96f1e4d76704c1a5d599e4429357
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2023-12-21 10:37:47 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231221/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,11 @@
+A = list(input())
+
+cnt = 0
+length = len(A)
+for i in range(1, length):
+    if A[i] == '(':
+        for j in range(i+1, length):
+            if A[j] == ')':
+                cnt += 1
+
+print(cnt)
