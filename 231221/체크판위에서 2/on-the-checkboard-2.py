a2883d80ca8b75531d17c86800b1a093e43a53a7
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2023-12-21 11:46:00 +0900

    Title: 체크판위에서 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231221/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,22 @@
+R, C = tuple(map(int, input().split()))
+grid = [list(input().split())
+        for _ in range(R)]
+
+current = grid[0][0]
+history = [(0,0)]
+cnt = 0
+
+if current == grid[R-1][C-1]:
+    print(cnt)
+else:
+    first = []
+    for i in range(1, R-1):
+        for j in range(1, C-1):
+            if current != grid[i][j]:
+                first.append((i, j))
+    for x, y in first:
+        for i in range(x+1, R-1):
+            for j in range(y+1, R-1):
+                if grid[x][y] != grid[i][j]:
+                    cnt += 1
+print(cnt)
