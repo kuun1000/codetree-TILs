9c1a0ac917b639c0faf40f76d164889bb5918c71
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2023-12-22 13:31:34 +0900

    Title: 원 모양으로 되어있는 방, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231222/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(input())
+arr = [int(input())
+        for _ in range(n)
+]
+
+min_dist = sys.maxsize
+for i in range(0, n):
+    dist = 0
+    prev_idx = i
+    for j in range(i+1, n+i):
+        # print(prev_idx, j%n)
+        # print(f"{arr[j%n]} * {abs(j-i)} = {arr[j%n]*abs(j-i)}") 
+        dist += arr[j%n]*abs(j-i)
+        # print(dist)
+        prev_idx = j%n
+    min_dist = min(min_dist, dist)
+    # print(min_dist)
+    # print()
+print(min_dist)
