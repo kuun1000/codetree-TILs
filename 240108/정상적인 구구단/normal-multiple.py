917dc4ee89cc3ef3cf6afd30358dd1e80de4eb22
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-01-08 20:55:54 +0900

    Title: 정상적인 구구단, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240108/정상적인 구구단/normal-multiple.py
+++ 240108/정상적인 구구단/normal-multiple.py
@@ -1,9 +1,11 @@
 # 1. 정수 n 입력받기
 n = int(input())
 
+# 2. n번째 단 출력하기
 for i in range(1, n+1):
+    # 3. n x n 번째 출력하기
     for j in range(1, n+1):
         print(f"{i} * {j} = {i * j}", end="")
-        if j != n:
+        if j != n: # 마지막 단이 아닌 경우에만 끝에 ", " 출력하기 
             print(",", end=" ")
     print()
