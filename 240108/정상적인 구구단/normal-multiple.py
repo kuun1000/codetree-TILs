cdf001dd4fd9c7a5b06e4d6ab2f03f658e86ab1e
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-01-08 20:51:59 +0900

    Title: 정상적인 구구단, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240108/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,9 @@
+# 1. 정수 n 입력받기
+n = int(input())
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(f"{i} * {j} = {i * j}", end="")
+        if j != n:
+            print(",", end=" ")
+    print()
