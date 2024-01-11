681e52a3f826cbf12a11aa794ae2bd31dd49bfeb
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-01-11 22:01:49 +0900

    Title: 줄 세우기, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240111/줄 세우기/line-up-students.py
+++ 240111/줄 세우기/line-up-students.py
@@ -3,16 +3,16 @@ class Student:
     def __init__(self, height, weight, index):
         self.height = height 
         self.weight = weight
-        self.number = index
+        self.index = index
 
 # n 입력 받기
 n = int(input())
 
 # 학생 정보 입력 받기
 students = []
-for i in range(n):
+for i in range(1, n+1):
     h, w = list(map(int, input().split()))
-    students.append(Student(h, w, i+1))
+    students.append(Student(h, w, i))
 
 # 키: 내림차순, 몸무게: 내림차순, 번호: 오름차순 정렬
 students.sort(key=lambda x:(-x.height, -x.weight, x.index))
