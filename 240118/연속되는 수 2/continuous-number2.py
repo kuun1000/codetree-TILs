921c12ec46d37b4d7cd458992d765af17046a532
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-01-18 19:43:38 +0900

    Title: 연속되는 수 2, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240118/연속되는 수 2/continuous-number2.py
+++ 240118/연속되는 수 2/continuous-number2.py
@@ -3,10 +3,27 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 max_cnt = 0 # 최대 횟수
-cnt = 0 # 동일한 숫자가 나오는 횟수 
-for i in range(n):
+# cnt = 0 # 동일한 숫자가 나오는 횟수 
+# for i in range(n):
+#     # 2(1)번째 원소부터 현재 값과 이전값 같은 경우
+#     if i > 0 and arr[i-1] == arr[i]:
+#         # 해당 그룹에 대해 횟수 1 증가
+#         cnt += 1
+#     # 1(0)번째 원소 또는 현재값과 이전값 다른 경우
+#     else:
+#         # 새로운 그룹 -> 횟수 1로 초기화
+#         cnt = 1
+    
+#     # 현재 최대 횟수와 현재 그룹의 횟수 중 최대값
+#     max_cnt = max(max_cnt, cnt)
+
+# # 정답 출력
+# print(max_cnt)
+
+cnt = 1 # 동일한 숫자가 나오는 횟수 
+for i in range(1, n):
     # 2(1)번째 원소부터 현재 값과 이전값 같은 경우
-    if i > 0 and arr[i-1] == arr[i]:
+    if arr[i-1] == arr[i]:
         # 해당 그룹에 대해 횟수 1 증가
         cnt += 1
     # 1(0)번째 원소 또는 현재값과 이전값 다른 경우
