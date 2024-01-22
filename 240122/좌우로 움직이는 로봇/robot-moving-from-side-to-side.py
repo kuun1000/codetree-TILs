9ad5c11c7f6cc5e03eee925d299066a60204d0e3
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-01-22 15:03:19 +0900

    Title: 좌우로 움직이는 로봇, Time: 284ms, Memory: 41MB, Status: Passed - CodeTree

--- 240122/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240122/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,12 +1,12 @@
 MAX_D = 1000000
+
 n, m = tuple(map(int, input().split()))
 pos_a, pos_b = [0] * (MAX_D + 1), [0] * (MAX_D + 1)
 
 ## A
 time_a = 1  # 현재 시간(t)
 for _ in range(n):
-    t, d = input().split()
-    t = int(t)
+    t, d = tuple(input().split())
     # t초 동안, 이전 값 + 방향에 따른 값 (R: +1, L: -1)
     for _ in range(int(t)):
         pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
@@ -14,10 +14,8 @@ for _ in range(n):
 
 ## B
 time_b = 1  # 현재 시간(t)
-for _ in range(n):
-    t, d = input().split()
-    t = int(t)
-    
+for _ in range(m):
+    t, d = tuple(input().split())
     # t초 동안, 이전 값 + 방향에 따른 값 (R: +1, L: -1)
     for _ in range(int(t)):
         pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
@@ -25,21 +23,21 @@ for _ in range(n):
 
 
 ## 보정
-diff = len(pos_a) - len(pos_b)
-if diff > 0:
-    pos_b += [pos_b[time_b] * diff]
-elif diff < 0:
-    pos_a += [pos_a[time_a] * diff]
+if time_a > time_b:
+    for i in range(time_b, time_a):
+        pos_b[i] = pos_b[i-1]
+
+elif time_a < time_b:
+    for i in range(time_a, time_b):
+        pos_a[i] = pos_a[i-1]
 
 
 ## 위치 비교 
-prev_pos, cur_pos = 0, 0
+time_max = max(time_a, time_b)
 cnt = 0
-for i in range(1, len(pos_a)):
-    prev_pos = int(pos_a[i-1] != pos_b[i-1])    # 이전 위치 다른지
-    cur_pos = int(pos_a[i] == pos_b[i])         # 현재 위치 같은지
-
-    if prev_pos and cur_pos:
+for i in range(1, time_max):
+    # 이전 위치 다른지 & 현재 위치 같은지
+    if pos_a[i - 1] != pos_b[i - 1] and pos_a[i] == pos_b[i]:
         cnt += 1
 
 print(cnt)
