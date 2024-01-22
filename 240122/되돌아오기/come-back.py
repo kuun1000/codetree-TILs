975c43220ca6fc2d6b7c76f336070e847c1f9b41
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2024-01-22 15:58:10 +0900

    Title: 되돌아오기, Time: 165ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240122/되돌아오기/come-back.py
@@ -0,0 +1,29 @@
+dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1]
+mapper = {'E': 0, 'S': 1, 'W': 2, 'N': 3} # 동, 서, 남, 북
+
+N = int(input())
+
+x, y = 0, 0  # 현재 위치
+dir_num = 0  # 이동 방향
+sec = 0      # 걸린 시간
+answer = -1  # 정답 
+
+check = False # 시작점으로 돌아왔는지
+
+for _ in range(N):
+    direction, distance = input().split()
+    dir_num = mapper[direction]
+    distance = int(distance)
+
+    for _ in range(distance):
+        sec += 1
+        x, y = x + dxs[dir_num], y + dys[dir_num]
+
+        if x == 0 and y == 0: # 시작점으로 돌아온 경우
+            answer = sec
+            check = True
+
+    if check:
+        break
+
+print(answer)
