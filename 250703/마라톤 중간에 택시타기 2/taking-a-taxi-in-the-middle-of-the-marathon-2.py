1190e9ae6cf655ae9a9d91d991a2dad4c60f3567
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-07-03 23:40:54 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250703/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,20 @@
+import sys
+INT_MAX = sys.maxsize
+
+n = int(input())
+arr = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+ans = INT_MAX
+for i in range(1, n-1):
+    dist = 0
+    prev_idx = 0
+    for j in range(1, n):
+        if j == i:
+            continue
+        dist += abs(arr[prev_idx][0] - arr[j][0]) + abs(arr[prev_idx][1] + arr[j][1])
+        prev_idx = j
+
+    ans = min(ans, dist)
+
+print(ans)
