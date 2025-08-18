d6f70cc50758a9f9efb71ee438c5d22a6c05bbce
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-08-19 08:52:45 +0900

    Title: 원형 순열에서의 인원 제거, Time: 138ms, Memory: 25MB, Status: Passed - Codetree

---
+++ 250819/원형 순열에서의 인원 제거/josephus-permutations.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.dq = deque()
+
+    def push(self, item):
+        self.dq.append(item)
+
+    def empty(self):
+        return not self.dq
+
+    def size(self):
+        return len(self.dq)
+
+    def pop(self):
+        if not self.empty():
+            return self.dq.popleft()
+
+    def front(self):
+        if not self.empty():
+            return self.dq[0]
+
+n, k = list(map(int, input().split()))
+
+q = Queue()
+for i in range(1, n+1):
+    q.push(i)
+
+while q.size() != 1:
+    for i in range(1, k):
+        q.push(q.front())
+        q.pop()
+    print(q.pop(), end=" ")
+print(q.front())
