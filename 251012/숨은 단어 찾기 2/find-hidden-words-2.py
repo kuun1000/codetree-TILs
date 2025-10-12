242a4d09c49f5788a035f549d9cb4c4edc6d7c8c
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-12 18:21:55 +0900

    Title: 숨은 단어 찾기 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251012/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 251012/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -4,16 +4,25 @@ grid = [list(input()) for _ in range(n)]
 directions = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
 
 result = 0
-search = 'LEE'
 for i in range(n):
     for j in range(m):
+        # 첫 글자가 L이 아닌 경우 제외
+        if grid[i][j] != 'L':
+            continue
+
         for dx, dy in directions:
-            if not (0 <= i+dx*2 < n and 0 <= j+dy*2 < m):
-                continue
-            flag = True
-            for k in range(3):
-                flag &= search[k] == grid[i + dx*k][j + dy*k]
-            if flag:
+            curt = 1
+            cx, cy = i, j
+            while True:
+                nx, ny = cx + dx, cy + dy
+                if not (0 <= nx < n and 0 <= ny < m):
+                    break
+                if grid[nx][ny] != 'E':
+                    break
+                curt += 1
+                cx, cy = nx, ny
+            
+            if curt >= 3:
                 result += 1
 
 print(result)
