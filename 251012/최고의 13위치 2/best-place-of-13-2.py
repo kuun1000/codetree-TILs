11febcfd74871007f0f3d9385739043eca97c64b
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-12 18:39:59 +0900

    Title: 최고의 13위치 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251012/최고의 13위치 2/best-place-of-13-2.py
+++ 251012/최고의 13위치 2/best-place-of-13-2.py
@@ -6,7 +6,7 @@ for i in range(n):
     for j in range(n-2):
         for k in range(i+1, n):
             for l in range(n-2):
-                if i == k and not abs(j - l) > 2:
+                if i == k and not abs(j - l) <= 2:
                     continue
                 sum_coins = grid[i][j] + grid[i][j+1] + grid[i][j+2] + grid[k][l] + grid[k][l+1] + grid[k][l+2]
                 result = max(result, sum_coins)
