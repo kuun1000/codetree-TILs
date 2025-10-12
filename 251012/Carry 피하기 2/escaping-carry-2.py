898a377f843fc603f6d90108a9329a0a9a305597
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-12 17:42:34 +0900

    Title: Carry 피하기 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251012/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,20 @@
+import sys
+
+def no_carry(a, b, c):
+    a, b, c = a.zfill(5), b.zfill(5), c.zfill(5)
+    for i in range(5):
+        if int(a[i]) + int(b[i]) + int(c[i]) >= 10:
+            return False
+    return True
+
+n = int(input())
+arr = list(input() for _ in range(n))
+
+result = -sys.maxsize 
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            if no_carry(arr[i], arr[j], arr[k]):
+                result = max(result, int(arr[i])+int(arr[j])+int(arr[k]))
+
+print(result)
