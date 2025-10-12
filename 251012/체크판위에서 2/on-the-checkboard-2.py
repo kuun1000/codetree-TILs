d8e8a78a65f737843c234140a3f10790d931abe5
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-12 16:33:10 +0900

    Title: 체크판위에서 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251012/체크판위에서 2/on-the-checkboard-2.py
+++ 251012/체크판위에서 2/on-the-checkboard-2.py
@@ -2,8 +2,8 @@ r, c = list(map(int, input().split()))
 grid = [list(input().split()) for _ in range(r)]
 
 result = 0
-for i in range(r):
-    for j in range(c):
+for i in range(1, r):
+    for j in range(1, c):
         for k in range(i+1, r-1):
             for l in range(j+1, c-1):
                 if grid[0][0] != grid[i][j] and grid[i][j] != grid[k][l] and grid[k][l] != grid[r-1][c-1]:
