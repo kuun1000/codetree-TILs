bf6f9361694e9ceb19a53b7997902a3dea1436d2
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-14 05:49:06 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 270ms, Memory: 24MB, Status: Passed - Codetree

--- 251014/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 251014/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -2,12 +2,14 @@ n = int(input())
 a1, b1, c1 = list(map(int, input().split()))
 a2, b2, c2 = list(map(int, input().split()))
 
+def near(x, y):
+    return min(abs(x - y), n - abs(x - y)) <= 2
+
 def is_distance(i, j, k):
-    if (abs(a1-i) <= 2 or abs(a1-i) >= n-2) and (abs(b1-j) <= 2 or abs(b1-j) >= n-2) and (abs(c1-k) <= 2 or abs(c1-k) >= n-2):
-        return True
-    if (abs(a2-i) <= 2 or abs(a2-i) >= n-2) and (abs(b2-j) <= 2 or abs(b2-j) >= n-2) and (abs(c2-k) <= 2 or abs(c2-k) >= n-2):
-        return True
-    return False
+    return (
+        (near(i, a1) and near(j, b1) and near(k, c1)) or
+        (near(i, a2) and near(j, b2) and near(k, c2))
+    )
 
 ans = 0
 for i in range(1, n+1):
