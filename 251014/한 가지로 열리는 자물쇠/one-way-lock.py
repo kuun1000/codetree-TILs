b2cf5a43fde4c065fdb2dcd32dff1b58f50b11eb
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-14 05:29:56 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 251014/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 251014/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -2,15 +2,15 @@ n = int(input())
 a, b, c = list(map(int, input().split()))
 
 def is_distance(i, j, k):
-    if abs(i - a) > 2 or abs(j - b) > 2 or abs(k - c) > 2:
+    if abs(i - a) <= 2 or abs(j - b) <= 2 or abs(k - c) <= 2:
         return True
     return False
 
 ans = n * n * n 
-for i in range(1, 10):
-    for j in range(0, 10):
-        for k in range(0, 10):
-            if i == j == k and is_distance(i, j, k):
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        for k in range(1, n+1):
+            if not is_distance(i, j, k):
                 ans -= 1
 
 print(ans)
