9ef94512de39753c67b9625c75dd832a45145fc0
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-13 18:34:40 +0900

    Title: G or H 3, Time: 106ms, Memory: 18MB, Status: Passed - Codetree

--- 251013/G or H 3/G-or-H-3.py
+++ 251013/G or H 3/G-or-H-3.py
@@ -6,13 +6,13 @@ for _ in range(n):
     x.append(int(pos))
     c.append(char)
 
-positions = [0] * (max(x) + 1)
+positions = [0] * (10001)
 
 for pos, char in zip(x, c):
     positions[pos] = 1 if char == 'G' else 2
 
 max_score = 0
-for i in range(1, max(x) - k + 1):
+for i in range(1, 10001 - k + 1):
     score = sum(positions[i:i+k+1])
     max_score = max(max_score, score)
 
