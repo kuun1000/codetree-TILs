90ed2c19b25369f866c21b39a0b9e9da9729b51d
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-13 18:54:56 +0900

    Title: 아름다운 수열 2, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 251013/아름다운 수열 2/beautiful-sequence-2.py
+++ 251013/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,11 +1,24 @@
+from collections import Counter 
+
 n, m = list(map(int, input().split()))
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
+cb = Counter(b)
+ca = Counter(a[:m])
 ans = 0
-for i in range(n-m+1):
-    seq = a[i:i+m]
-    if len(set(seq) - set(b)) == 0:
+
+if ca == cb:
+    ans += 1
+
+for i in range(m, n):
+    ca[a[i]] += 1           # 새로 들어온 수 추가
+    ca[a[i - m]] -= 1       # 빠지는 수 제거
+    
+    if ca[a[i - m]] == 0:   # 0인 항목은 삭제
+        del ca[a[i - m]]
+    
+    if ca == cb:
         ans += 1
 
-print(ans)            
+print(ans)
