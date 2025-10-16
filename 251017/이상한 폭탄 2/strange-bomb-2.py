c0c4f4d39867e08e2b427aa6928de38aa667d46f
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-17 08:46:21 +0900

    Title: 이상한 폭탄 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251017/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,12 @@
+n, k = list(map(int, input().split()))
+arr = [int(input()) for _ in range(n)]
+
+ans = -1
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            continue
+        if arr[i] == arr[j] and abs(i - j) <= k:
+            ans = max(ans, arr[i])
+
+print(ans)
