edd3d1e75d22077ca3377c0f3a847b52d7c275e5
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-20 08:34:51 +0900

    Title: 스승의 은혜 3, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251020/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 251020/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,24 +1,26 @@
 n, b = list(map(int, input().split()))
 arr = [tuple(map(int, input().split())) for _ in range(n)]
 
-arr.sort(key=lambda x:(x[0]//2 + x[1]))
+arr.sort(key=lambda x: sum(x))
 
-ans = -1
+ans = 0
 for i in range(n):
     money = arr[i][0] // 2 + arr[i][1]
     cnt = 1
-    if money > b:
-        break
     
+    tmp = []
     for j in range(n):
-        if i == j:
-            continue
-        money += sum(arr[j])
-        
-        if money > b:
-            money -= sum(arr[j])
+        if i != j:
+            tmp.append(sum(arr[j]))
+    tmp.sort()
+
+    for cost in tmp:
+        if money + cost < b:
+            money += cost
+            cnt += 1
+        else:
             break
-        cnt += 1
+
     ans = max(ans, cnt)
 
 print(ans)
