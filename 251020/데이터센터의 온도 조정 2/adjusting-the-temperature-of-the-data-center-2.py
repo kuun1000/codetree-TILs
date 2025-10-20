1c160d6e20a4242eaf3854d9b2580abe19029f09
Yeonsu Lee <64531324+kuun1000@users.noreply.github.com>
2025-10-20 09:07:02 +0900

    Title: 데이터센터의 온도 조정 2, Time: 54ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251020/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,24 @@
+import sys
+
+n, c, g, h = list(map(int, input().split()))
+
+min_prefs, max_prefs = [], []
+for _ in range(n):
+    a, b = list(map(int, input().split()))
+    min_prefs.append(a)
+    max_prefs.append(b)
+min_range, max_range = min(min_prefs), max(max_prefs)
+
+ans = -sys.maxsize
+for temp in range(min_range, max_range+1):
+    workload = 0
+    for i in range(n):
+        if temp < min_prefs[i]:
+            workload += c
+        elif min_prefs[i] <= temp <= max_prefs[i]:
+            workload += g
+        elif temp > max_prefs[i]:
+            workload += h
+    ans = max(ans, workload)
+
+print(ans)
